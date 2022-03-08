1672f823dcee25ef999d1a3003b588c22cbc82c7
Artak <artak.ashugatoyan@gmail.com>
2022-03-08 17:51:07 +0400

    answer

---
+++ dist/main.js
@@ -0,0 +1,22 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const ucraftMainTeams = {
+  hoory: "good team",
+  builder: "cool team",
+  ucCommerce: "best team :D",
+};
+
+const ucraftAllTeams = {
+  ...ucraftMainTeams,
+  springBuilder: "really big team",
+  aiTeam: "Levon is master jedi",
+};
+
+console.log(ucraftMainTeams);
+console.log(ucraftAllTeams);
+
+/******/ })()
+;
